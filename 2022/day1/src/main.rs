e646a7834d8661c94f4d62c277e5d7f3df47af70
Artoghrul Gahramanli <artogahr@gmail.com>
2022-12-01 23:55:38 +0100

    day 1 part 2

--- 2022/day1/src/main.rs
+++ 2022/day1/src/main.rs
@@ -3,10 +3,11 @@ use std::io::Write;
 
 fn main() {
     let mut max_number: i32 = 0;
+    let mut second_max: i32 = 0;
+    let mut third_max: i32 = 0;
     let mut current_number: i32 = 0;
     let mut is_end_of_input: bool = false;
     loop {
-        print!("prompt> ");
         io::stdout().flush().expect("couldn't flush");
         let mut str = String::new();
         io::stdin().read_line(&mut str).expect("couldn't read");
@@ -18,17 +19,38 @@ fn main() {
         if input_is_numeric {
             is_end_of_input = false;
             current_number += str.to_string().parse::<i32>().unwrap();
-            if current_number > max_number {
-                max_number = current_number;
-            }
         }
         else {
             if is_end_of_input {
                 break;
             } 
+                        if current_number > third_max {
+                if current_number > second_max {
+                    if current_number > max_number {
+                        third_max = second_max;
+                        second_max = max_number;
+                        max_number = current_number;
+                        //println!("max number became {max_number}");
+                        //println!("so second number became {second_max}");
+                        //println!("so third number became {third_max}");
+                    }
+                    else {
+                        third_max = second_max;
+                        second_max = current_number;
+                        // println!("second number became {second_max}");
+                        // println!("so third number became {third_max}");
+                    }
+                }
+                else {
+                    third_max = current_number;
+                        // println!("third number became {third_max}");
+                }
+            }
+
             current_number = 0;
             is_end_of_input = true;
         }
     }
-    println!("{max_number}");
+    let maxi = max_number + second_max + third_max;
+    println!("{maxi}");
 }
