cddca3fa033437eeb8a56500bf12391f3672467c
Artoghrul Gahramanli <artogahr@gmail.com>
2022-12-03 00:14:14 +0100

    delete target dir

--- 2022/day2/target/debug/day2.d
+++
@@ -1,1 +0,0 @@
-/home/agahramanli/workplace/advent-of-code/2022/day2/target/debug/day2: /home/agahramanli/workplace/advent-of-code/2022/day2/src/main.rs
--- 2022/day2/target/debug/deps/day2-97f5959149b177de.d
+++
@@ -1,5 +0,0 @@
-/home/agahramanli/workplace/advent-of-code/2022/day2/target/debug/deps/day2-97f5959149b177de: src/main.rs
-
-/home/agahramanli/workplace/advent-of-code/2022/day2/target/debug/deps/day2-97f5959149b177de.d: src/main.rs
-
-src/main.rs:
