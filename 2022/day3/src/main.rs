470888c4b02b6da8db884105053248959568005c
Artoghrul Gahramanli <artogahr@gmail.com>
2022-12-04 01:15:36 +0100

    2022 day 3 part 2

--- 2022/day3/src/main.rs
+++ 2022/day3/src/main.rs
@@ -1,47 +1,42 @@
 use std::io::{self, BufRead};
 
 fn main() -> io::Result<()> {
+
+    println!("Tip: remember to press newline a couple of times after entering input");
+
     let mut lines = io::stdin().lock().lines();
-    let mut user_input = String::new();
     let mut sum: u32 = 0;
 
-    'lineread: while let Some(line) = lines.next() {
-        let last_input = line.unwrap();
+    'lineread: while let Some(line1) = lines.next() {
+        let line1 = line1.unwrap();
+        let line2 = lines.next().unwrap()?;
+        let line3 = lines.next().unwrap()?;
 
-        // stop reading
-        if last_input.is_empty() {
-            break;
+        let line1_chars: Vec<char> = line1.trim().chars().collect();
+        let line2_chars: Vec<char> = line2.chars().collect();
+        let line3_chars: Vec<char> = line3.chars().collect();
+
+        if line1_chars.is_empty() || line2_chars.is_empty() || line3_chars.is_empty() {
+            break 'lineread;
         }
 
-        let char_vec: Vec<char> = last_input.chars().collect();
-        if char_vec.is_empty() {
-            break;
+        for char in line1_chars {
+            if line2_chars.contains(&char) && line3_chars.contains(&char) {
+                    if (char as u32) < 97 {
+                        let value = (char as u32) - 38;
+                        sum += value;
+                        // println!("found char {0}, adding {1}", char, value);
+                        continue 'lineread;
+                    }
+                    else {
+                        let value = (char as u32) - 96;
+                        sum += value;
+                        // println!("found char {0}, adding {1}", char, value);
+                        continue 'lineread;
+                    }
+            }
         }
-        let (first, second) = char_vec.split_at(char_vec.len()/2);
-
-        for char in first {
-            if second.contains(char) {
-                if (*char as u32) < 97 {
-                    let value = (*char as u32) - 38;
-                    sum += value;
-                    println!("found char {0}, adding {1}", char, value);
-                    continue 'lineread;
-                }
-                else {
-                    let value = (*char as u32) - 96;
-                    sum += value;
-                    println!("found char {0}, adding {1}", char, value);
-                    continue 'lineread;
-                }
-
-}
-        }
-
-        // add a new line once user_input starts storing user input
     }
-
     println!("Sum: {0}", sum);
-
-    // the lock is released after it goes out of scope
     Ok(())
 }
