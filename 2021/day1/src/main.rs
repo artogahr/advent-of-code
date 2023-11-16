2f407ce9936dcfa07708f1d4a09ee720f3fa0412
Arto Gahr <artogahr@gmail.com>
2023-11-16 22:43:34 +0100

    2021 day 1 part 2

--- 2021/day1/src/main.rs
+++ 2021/day1/src/main.rs
@@ -1,29 +1,43 @@
 use std::io;
 
 fn main() {
-    let mut currentNum = 0;
-    let mut previousNum = 0;
-    let mut counter = 0;
+    let mut numbers: Vec<i32> = vec![];
+    let mut currentIter = 1;
+    let mut previousIter = 0;
+    let mut count = 0;
     loop {
         let mut input_text = String::new();
         io::stdin()
             .read_line(&mut input_text)
             .expect("failed to read from stdin");
+
         if input_text.is_empty() {
             break;
         }
-    
+
         let trimmed = input_text.trim();
-        match trimmed.parse::<u32>() {
-            Ok(i) => currentNum = i,
+        match trimmed.parse::<i32>() {
+            Ok(i) => {
+                numbers.push(i);
+            }
             Err(..) => {
                 break;
-            },
+            }
         };
-        if currentNum > previousNum {
-            counter += 1;
-        }
-        previousNum = currentNum;
     }
-    print!("{}", counter-1);
+    loop {
+        let currentIterSum =
+            numbers[currentIter] + numbers[currentIter + 1] + numbers[currentIter + 2];
+        let prevItemSum =
+            numbers[previousIter] + numbers[previousIter + 1] + numbers[previousIter + 2];
+        if currentIterSum > prevItemSum {
+            count += 1;
+        }
+        if currentIter + 3 == numbers.len() {
+            break;
+        }
+        currentIter += 1;
+        previousIter += 1;
+    }
+    println!("{}", count);
 }
