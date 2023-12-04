56f1c4f327baef7bcb59a296d6d23aea7684c62f
Arto Gahr <artogahr@gmail.com>
2023-12-04 09:41:01 +0100

    2023 day 3 part 1

--- 2023/day3/src/main.rs
+++ 2023/day3/src/main.rs
@@ -1,8 +1,11 @@
-use std::collections::HashMap;
 
+use std::vec;
+
+#[derive(Debug)]
 struct Number {
-    start_index: u32,
-    end_index: u32, 
+    start_index: usize,
+    end_index: usize,
+    line_index: usize,
     value: u32,
 }
 
@@ -14,16 +17,66 @@ fn parse_input() -> Vec<String> {
         .collect()
 }
 
-fn part1(input: Vec<String>) -> i64 {
-    let numbers: Vec<Number> = vec![];
-    for line in input { 
-        let line = line.replace(".", " ");
-        dbg!(line);
+fn part1(input: Vec<String>) -> u32 {
+    //empty chars 2d array, 1 extra wall in each direction to avoid over/underflows
+    let mut chars: Vec<Vec<char>> = vec![vec!['.'; input[0].len() + 2]; input.len() + 2];
+    for (i, line) in input.iter().enumerate() {
+        for (j, ch) in line.chars().enumerate() {
+            chars[i + 1][j + 1] = ch;
+        }
     }
-    0
+    let mut numbers: Vec<Number> = vec![];
+    let mut is_reading_num = false;
+    let mut current_num: u32 = 0;
+    for (i, line) in chars.iter().enumerate() {
+        for (j, ch) in line.iter().enumerate() {
+            match ch {
+                ('0'..='9') => {
+                    if is_reading_num {
+                        current_num *= 10;
+                        current_num += ch.to_digit(10).unwrap();
+                    } else {
+                        current_num = ch.to_digit(10).unwrap();
+                    }
+                    is_reading_num = true;
+                }
+                _ => {
+                    if is_reading_num {
+                        is_reading_num = false;
+                        numbers.push(Number {
+                            start_index: j - current_num.to_string().len(),
+                            end_index: j - 1,
+                            line_index: i,
+                            value: current_num,
+                        });
+                        //dbg!(&numbers);
+                    }
+                }
+            }
+        }
+    }
+    //dbg!(&numbers);
+    let mut sum: u32 = 0;
+    'number_looop: for number in &numbers {
+        for line_i in number.line_index - 1..=number.line_index + 1 {
+            for char_i in number.start_index - 1..=number.end_index + 1 {
+                //dbg!(number.value, line_i, char_i, chars[line_i][char_i]);
+                if chars[line_i][char_i] != '.' && !chars[line_i][char_i].is_ascii_digit() {
+                    //println!(  "number {} has char {} in surrounding", number.value, chars[line_i][char_i]);
+                    sum += number.value;
+                    continue 'number_looop;
+                }
+            }
+        }
+        //println!("number {} has nothing in surrounding", number.value);
+    }
+    dbg!(&numbers.len());
+    sum
 }
 
-fn part2(input: Vec<String>) -> i64 {todo!();}
+fn part2(_input: Vec<String>) -> i64 {
+    todo!();
+}
 
 fn main() {
     let solution = advent::new(parse_input).part(part1).part(part2).build();
