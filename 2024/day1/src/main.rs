bb5d747706d63dd3a3cf33f77bdd035cc72cb6f3
Artoghrul Gahramanli <artogahr@gmail.com>
2024-12-01 17:45:27 +0100

    2024 day 1 part 2

--- 2024/day1/src/main.rs
+++ 2024/day1/src/main.rs
@@ -1,6 +1,7 @@
-use std::{fs::read_to_string, u32};
+use std::{collections::HashMap, fs::read_to_string, u32};
 fn main() {
     println!("Part 1: {}", part1());
+    println!("Part 2: {}", part2());
 }
 
 fn part1() -> u32 {
@@ -28,6 +29,36 @@ fn part1() -> u32 {
     let mut total: u32 = 0;
     for i in 0..left_side.len() {
         total += left_side[i].abs_diff(right_side[i]);
+    }
+    total
+}
+
+fn part2() -> u32 {
+    let input_lines = read_lines("input.txt");
+
+    let mut left_side: Vec<u32> = Vec::new();
+    let mut right_side_counts: HashMap<u32, u32> = HashMap::new();
+
+    for line in input_lines {
+        let (left, right) = line
+            .split_once(' ')
+            .map(|(l, r)| {
+                (
+                    l.trim().parse::<u32>().unwrap(),
+                    r.trim().parse::<u32>().unwrap(),
+                )
+            })
+            .unwrap();
+        left_side.push(left);
+        match right_side_counts.get(&right) {
+            Some(count) => right_side_counts.insert(right, count + 1),
+            None => right_side_counts.insert(right, 1),
+        };
+    }
+
+    let mut total: u32 = 0;
+    for i in 0..left_side.len() {
+        total += left_side[i] * right_side_counts.get(&left_side[i]).unwrap_or(&0);
     }
     total
 }
