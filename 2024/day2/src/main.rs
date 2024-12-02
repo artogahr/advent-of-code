d081eb8b195e83dd928cee8155bf910b4069bbef
Artoghrul Gahramanli <artogahr@gmail.com>
2024-12-02 22:55:06 +0100

    2024 day 2 part 2

--- 2024/day2/src/main.rs
+++ 2024/day2/src/main.rs
@@ -5,121 +5,71 @@ fn main() {
 }
 
 fn part1() -> u32 {
-    let input_lines = read_lines("input.txt");
+    let records = read_lines("input.txt");
 
     let mut safe_records_count: u32 = 0;
-    for line in input_lines {
-        let numbers: Vec<u32> = line
+    for record in records {
+        let record: Vec<u32> = record
             .split(' ')
             .map(|num| num.parse::<u32>().unwrap())
             .collect();
-
-        match numbers[0].cmp(&numbers[1]) {
-            std::cmp::Ordering::Less => {
-                if numbers.windows(2).all(|w| w[0] < w[1] && w[1] - w[0] <= 3) {
-                    safe_records_count += 1;
-                }
-            }
-            std::cmp::Ordering::Equal => {
-                continue;
-            }
-            std::cmp::Ordering::Greater => {
-                if numbers.windows(2).all(|w| w[0] > w[1] && w[0] - w[1] <= 3) {
-                    safe_records_count += 1;
-                }
-            }
-        };
+        if is_valid_record(&record) {
+            safe_records_count += 1;
+        }
     }
     safe_records_count
 }
 
 fn part2() -> u32 {
-    let input_lines = read_lines("input.txt");
+    let records = read_lines("input.txt");
 
     let mut safe_records_count: u32 = 0;
-    for line in input_lines {
-        let numbers: Vec<u32> = line
+
+    for record in records {
+        let record: Vec<u32> = record
             .split(' ')
-            .map(|num| num.parse::<u32>().unwrap())
+            .map(|level| level.parse::<u32>().unwrap())
             .collect();
 
-        match numbers[0].cmp(&numbers[1]) {
-            std::cmp::Ordering::Less => {
-                if numbers.windows(2).all(|w| w[0] < w[1] && w[1] - w[0] <= 3) {
+        if is_valid_record(&record) {
+            safe_records_count += 1;
+        } else {
+            'outer: for i in 0..record.len() {
+                let mut new_record: Vec<u32> = Vec::new();
+                for j in 0..record.len() {
+                    if i != j {
+                        new_record.push(record[j]);
+                    }
+                }
+                if is_valid_record(&new_record) {
                     safe_records_count += 1;
-                } else {
-                    for i in 0..numbers.len() {
-                        let mut new_numbers: Vec<u32> = Vec::new();
-                        for j in 0..numbers.len() {
-                            if i != j {
-                                new_numbers.push(numbers[j]);
-                            }
-                        }
-
-                        if new_numbers
-                            .windows(2)
-                            .all(|w| w[0] < w[1] && w[1] - w[0] <= 3)
-                        {
-                            safe_records_count += 1;
-                        }
-                    }
+                    break 'outer;
                 }
             }
-            std::cmp::Ordering::Equal => {
-                for i in 0..numbers.len() {
-                    let mut new_numbers: Vec<u32> = Vec::new();
-                    for j in 0..numbers.len() {
-                        if i != j {
-                            new_numbers.push(numbers[j]);
-                        }
-                    }
-
-                    match new_numbers[0].cmp(&new_numbers[1]) {
-                        std::cmp::Ordering::Less => {
-                            if new_numbers
-                                .windows(2)
-                                .all(|w| w[0] < w[1] && w[1] - w[0] <= 3)
-                            {
-                                safe_records_count += 1;
-                            }
-                        }
-                        std::cmp::Ordering::Equal => {
-                            continue;
-                        }
-                        std::cmp::Ordering::Greater => {
-                            if new_numbers
-                                .windows(2)
-                                .all(|w| w[0] > w[1] && w[0] - w[1] <= 3)
-                            {
-                                safe_records_count += 1;
-                            }
-                        }
-                    }
-                }
-            }
-            std::cmp::Ordering::Greater => {
-                if numbers.windows(2).all(|w| w[0] > w[1] && w[0] - w[1] <= 3) {
-                    safe_records_count += 1;
-                } else {
-                    for i in 0..numbers.len() {
-                        let mut new_numbers: Vec<u32> = Vec::new();
-                        for j in 0..numbers.len() {
-                            if i != j {
-                                new_numbers.push(numbers[j]);
-                            }
-                        }
-                        if new_numbers
-                            .windows(2)
-                            .all(|w| w[0] > w[1] && w[0] - w[1] <= 3)
-                        {
-                            safe_records_count += 1;
-                        }
-                    }
-                }
-            }
-        };
+        }
     }
+
     safe_records_count
+}
+
+fn is_valid_record(record: &Vec<u32>) -> bool {
+    match record[0].cmp(&record[1]) {
+        std::cmp::Ordering::Less => {
+            if record.windows(2).all(|w| w[0] < w[1] && w[1] - w[0] <= 3) {
+                true
+            } else {
+                false
+            }
+        }
+        std::cmp::Ordering::Equal => false,
+        std::cmp::Ordering::Greater => {
+            if record.windows(2).all(|w| w[0] > w[1] && w[0] - w[1] <= 3) {
+                true
+            } else {
+                false
+            }
+        }
+    }
 }
 
 fn read_lines(filename: &str) -> Vec<String> {
