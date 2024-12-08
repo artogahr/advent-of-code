d819670506e0410b3acfa4835ef4b5748c9cd791
Artoghrul Gahramanli <artogahr@gmail.com>
2024-12-08 19:00:26 +0100

    2024 day 8 part 2

--- 2024/day8/src/main.rs
+++ 2024/day8/src/main.rs
@@ -7,6 +7,7 @@ fn main() {
 
     let antenna_map: Vec<Vec<char>> = f.lines().map(|l| l.unwrap().chars().collect()).collect();
     part1(&antenna_map);
+    part2(&antenna_map);
 }
 
 fn part1(antenna_map: &Vec<Vec<char>>) {
@@ -49,6 +50,54 @@ fn part1(antenna_map: &Vec<Vec<char>>) {
                         && y2 < antenna_map[0].len() as i32
                     {
                         antinodes.insert((x2 as usize, y2 as usize));
+                    }
+                }
+            }
+        }
+    }
+    println!("Part 1: {0} total antinodes", antinodes.len());
+}
+fn part2(antenna_map: &Vec<Vec<char>>) {
+    let mut antennas: HashMap<char, Vec<(usize, usize)>> = HashMap::new();
+    let mut antinodes: HashSet<(usize, usize)> = HashSet::new();
+    for x in 0..antenna_map.len() {
+        for y in 0..antenna_map[0].len() {
+            let ch = antenna_map[x][y];
+            if ch != '.' {
+                antennas.entry(ch).or_insert_with(Vec::new).push((x, y));
+            }
+        }
+    }
+
+    for (_antenna, locations) in antennas {
+        for i in 0..locations.len() {
+            for j in 0..locations.len() {
+                if i != j {
+                    for k in 0.. {
+                        let (loc1_x, loc1_y) = (locations[i].0 as i32, locations[i].1 as i32);
+                        let (loc2_x, loc2_y) = (locations[j].0 as i32, locations[j].1 as i32);
+
+                        let x1 = loc1_x + k * (loc2_x - loc1_x);
+                        let y1 = loc1_y + k * (loc2_y - loc1_y);
+                        let x2 = loc2_x + k * (loc1_x - loc2_x);
+                        let y2 = loc2_y + k * (loc1_y - loc2_y);
+
+                        let bounds_x = antenna_map.len() as i32;
+                        let bounds_y = antenna_map[0].len() as i32;
+
+                        let valid1 = x1 >= 0 && y1 >= 0 && x1 < bounds_x && y1 < bounds_y;
+                        let valid2 = x2 >= 0 && y2 >= 0 && x2 < bounds_x && y2 < bounds_y;
+
+                        if valid1 {
+                            antinodes.insert((x1 as usize, y1 as usize));
+                        }
+                        if valid2 {
+                            antinodes.insert((x2 as usize, y2 as usize));
+                        }
+
+                        if !valid1 && !valid2 {
+                            break;
+                        }
                     }
                 }
             }
