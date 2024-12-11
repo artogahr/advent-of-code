2ffb29bda61544aecef5e88442ca3040191b7cba
Artoghrul Gahramanli <artogahr@gmail.com>
2024-12-11 21:26:35 +0100

    2024 day 11 part 1 and 2

--- 2024/day11/src/main.rs
+++ 2024/day11/src/main.rs
@@ -8,58 +8,78 @@ fn main() {
 }
 
 fn part1(found_dividers: &mut HashMap<u64, (u64, u64)>, found_multipliers: &mut HashMap<u64, u64>) {
-    let mut stones: Vec<u64> = fs::read_to_string("input.txt")
+    let stones: Vec<u64> = fs::read_to_string("input.txt")
         .unwrap()
         .trim()
         .split(" ")
         .map(|str| str.parse().unwrap())
         .collect();
 
+    let mut stones_map: HashMap<u64, usize> = HashMap::new();
+    stones.iter().for_each(|stone| {
+        stones_map.insert(*stone, 1);
+    });
+
     for _ in 0..25 {
-        stones = blink(&stones, found_dividers, found_multipliers);
+        stones_map = blink(&stones_map, found_dividers, found_multipliers);
     }
-    println!("Part 1: {0}", stones.len());
+    let result = stones_map
+        .iter()
+        .fold(0, |sum, (_number, count)| sum + count);
+
+    println!("Part 1: {result}");
 }
 
 fn part2(found_dividers: &mut HashMap<u64, (u64, u64)>, found_multipliers: &mut HashMap<u64, u64>) {
-    let mut stones: Vec<u64> = fs::read_to_string("input.txt")
+    let stones: Vec<u64> = fs::read_to_string("input.txt")
         .unwrap()
         .trim()
         .split(" ")
         .map(|str| str.parse().unwrap())
         .collect();
 
-    for i in 0..75 {
-        println!("{i}");
-        stones = blink(&stones, found_dividers, found_multipliers);
+    let mut stones_map: HashMap<u64, usize> = HashMap::new();
+    stones.iter().for_each(|stone| {
+        stones_map.insert(*stone, 1);
+    });
+
+    for _ in 0..75 {
+        stones_map = blink(&stones_map, found_dividers, found_multipliers);
     }
-    println!("Part 1: {0}", stones.len());
+    let result = stones_map
+        .iter()
+        .fold(0, |sum, (_number, count)| sum + count);
+
+    println!("Part 2: {result}");
 }
 
 #[inline(always)]
 fn blink(
-    stones: &Vec<u64>,
+    stones: &HashMap<u64, usize>,
     found_dividers: &mut HashMap<u64, (u64, u64)>,
     found_multipliers: &mut HashMap<u64, u64>,
-) -> Vec<u64> {
-    let mut new_stones: Vec<u64> = Vec::new();
-    for stone in stones {
+) -> HashMap<u64, usize> {
+    let mut new_stones: HashMap<u64, usize> = HashMap::new();
+    for (stone, count) in stones.clone() {
         match stone {
             0 => {
-                new_stones.push(1);
+                *new_stones.entry(1).or_insert(0) += count;
             }
             _ if stone.to_string().len() % 2 == 0 => {
-                let (stone1, stone2) = found_dividers.entry(*stone).or_insert({
+                let (stone1, stone2) = found_dividers.entry(stone).or_insert({
                     let midpoint = stone.to_string().len() / 2;
-                    let (stone1, stone2) = (stone / midpoint as u64, stone % midpoint as u64);
+                    let divider = 10_u64.pow(midpoint as u32);
+                    let (stone1, stone2) = (stone / divider as u64, stone % divider as u64);
                     (stone1, stone2)
                 });
 
-                new_stones.push(*stone1);
-                new_stones.push(*stone2);
+                *new_stones.entry(*stone1).or_insert(0) += count;
+                *new_stones.entry(*stone2).or_insert(0) += count;
             }
             _ => {
-                new_stones.push(*found_multipliers.entry(*stone).or_insert(stone * 2024));
+                *new_stones
+                    .entry(*found_multipliers.entry(stone).or_insert(stone * 2024))
+                    .or_insert(0) += count;
             }
         }
     }
