2da59c81af4e31a3292c7d0090822aa59feb5ab4
Artoghrul Gahramanli <artogahr@gmail.com>
2024-12-14 14:53:51 +0100

    2024 day 14 part 2

--- 2024/day14/src/main.rs
+++ 2024/day14/src/main.rs
@@ -1,5 +1,5 @@
 use core::panic;
-use std::{default, fs, thread::sleep, time::Duration};
+use std::{fs, thread::sleep, time::Duration, usize};
 
 use regex::Regex;
 
@@ -17,8 +17,8 @@ impl Robot {
 }
 
 fn main() {
-    println!("Hello, world!");
     println!("Part 1: {0}", part1(read_input()));
+    println!("Part 2: {0}", part2(read_input()));
 }
 
 fn part1(robots: Vec<Robot>) -> usize {
@@ -28,7 +28,7 @@ fn part1(robots: Vec<Robot>) -> usize {
         for robot in robots.iter_mut() {
             robot.move_one_second();
         }
-        print_map(&robots, 101, 103);
+        //print_map(&robots, 101, 103);
         //sleep(Duration::from_millis(300));
         //print!("{}[2J", 27 as char);
         //print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
@@ -53,7 +53,36 @@ fn part1(robots: Vec<Robot>) -> usize {
     q1 * q2 * q3 * q4
 }
 
-fn print_map(robots: &Vec<Robot>, x: usize, y: usize) {
+fn part2(original_robots: Vec<Robot>) -> usize {
+    let mut robots = original_robots.clone();
+    let mut lowest_score_index = 0;
+    let mut lowest_danger_score = usize::MAX;
+    for i in 1..103 * 107 {
+        for robot in robots.iter_mut() {
+            robot.move_one_second();
+        }
+        let mut q1 = 0;
+        let mut q2 = 0;
+        let mut q3 = 0;
+        let mut q4 = 0;
+        for robot in &robots {
+            match robot.position {
+                (x, y) if x < 101 / 2 && y < 103 / 2 => q1 += 1,
+                (x, y) if x >= 101 / 2 + 1 && y < 103 / 2 => q2 += 1,
+                (x, y) if x < 101 / 2 && y >= 103 / 2 + 1 => q3 += 1,
+                (x, y) if x >= 101 / 2 + 1 && y >= 103 / 2 + 1 => q4 += 1,
+                _ => {}
+            };
+        }
+        let danger_score = q1 * q2 * q3 * q4;
+        if danger_score < lowest_danger_score {
+            lowest_danger_score = danger_score;
+            lowest_score_index = i;
+        }
+    }
+    lowest_score_index
+}
+fn _print_map(robots: &Vec<Robot>, x: usize, y: usize) {
     let mut map: Vec<Vec<usize>> = vec![vec![0; x]; y];
 
     println!("-----------------");
@@ -70,6 +99,9 @@ fn print_map(robots: &Vec<Robot>, x: usize, y: usize) {
         }
         println!();
     }
+    sleep(Duration::from_millis(300));
+    print!("{}[2j", 27 as char);
+    print!("{esc}[2j{esc}[1;1h", esc = 27 as char);
 }
 
 fn read_input() -> Vec<Robot> {
