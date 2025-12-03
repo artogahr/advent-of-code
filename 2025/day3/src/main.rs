e1dc1806cbef73b3b508b0af246e3b66b1ef4ea3
Arto Gahr <artogahr@gmail.com>
2025-12-03 21:22:57 +0100

    2025 day 3 part 2

--- 2025/day3/src/main.rs
+++ 2025/day3/src/main.rs
@@ -7,22 +7,27 @@ fn main() {
         .map(|line| line.chars().map(|ch| ch.to_digit(10).unwrap()).collect())
         .collect();
 
-    let mut total = 0;
+    let mut total: u64 = 0;
 
     for bank in banks {
-        let mut max_index = 0;
-        for i in 1..bank.len() - 1 {
-            if bank[i] > bank[max_index] {
-                max_index = i;
+        let mut current_max_index = 0;
+
+        // println!("Checking bank: {:?}", bank);
+
+        for digit in (0..12).rev() {
+            // print!("Checking digit no {digit}: ");
+            for i in current_max_index + 1..bank.len() - digit {
+                if bank[i] > bank[current_max_index] {
+                    current_max_index = i;
+                }
             }
+            // println!(
+            //     "found at index {current_max_index}: {}",
+            //     bank[current_max_index]
+            // );
+            total += 10_u64.pow(digit as u32) * bank[current_max_index] as u64;
+            current_max_index += 1;
         }
-        let mut second_max_index = max_index + 1;
-        for j in max_index + 2..bank.len() {
-            if bank[j] > bank[second_max_index] {
-                second_max_index = j;
-            }
-        }
-        total += 10 * bank[max_index] + bank[second_max_index];
     }
 
     dbg!(total);
